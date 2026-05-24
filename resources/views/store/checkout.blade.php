bc7cb5b8e785a5eeb64a3c8373a378ac76319de9
Priyanshu7644 <priyanshukr988@gmail.com>
2026-05-25 03:52:58 +0530

    fix: resolve broken image paths on checkout page summary

--- resources/views/store/checkout.blade.php
+++ resources/views/store/checkout.blade.php
@@ -133,10 +133,17 @@
                             @php $total += $details['price'] * $details['quantity'] @endphp
                             <li class="flex py-4">
                                 <div class="h-16 w-16 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                    @if(isset($details['image']) && $details['image'])
-                                        <img src="{{ str_starts_with($details['image'], 'http') ? $details['image'] : Storage::url($details['image']) }}" class="h-full w-full object-cover">
+                                    @php
+                                        $imgSrc = str_starts_with($details['image'] ?? '', '/images/')
+                                            ? asset($details['image'])
+                                            : (str_starts_with($details['image'] ?? '', 'http') ? $details['image'] : asset('storage/' . ($details['image'] ?? '')));
+                                    @endphp
+                                    @if(!empty($details['image']))
+                                        <img src="{{ $imgSrc }}" class="h-full w-full object-cover" alt="{{ $details['name'] }}">
                                     @else
-                                        <div class="w-full h-full bg-gray-100"></div>
+                                        <div class="w-full h-full bg-gray-100 flex items-center justify-center">
+                                            <svg class="w-6 h-6 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                                        </div>
                                     @endif
                                 </div>
                                 <div class="ml-4 flex flex-1 flex-col">
